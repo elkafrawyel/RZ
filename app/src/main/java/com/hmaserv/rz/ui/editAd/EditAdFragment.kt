613a7057dbbcb8efae8ed237c9efb48f7a17f47e
elfiky <elfiky-work@hmaserv.com>
2019-01-20 17:45:18 +0200

    Add editAd attributes

--- app/src/main/java/com/hmaserv/rz/ui/editAd/EditAdFragment.kt
+++ app/src/main/java/com/hmaserv/rz/ui/editAd/EditAdFragment.kt
@@ -5,10 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.hmaserv.rz.R
+import com.hmaserv.rz.domain.Ad
+import com.hmaserv.rz.domain.UiState
+import com.hmaserv.rz.ui.BaseFragment
+import com.hmaserv.rz.ui.createAd.AdImagesAdapter
+import com.hmaserv.rz.ui.createAd.AttributesAdapter
+import kotlinx.android.synthetic.main.edit_ad_fragment.*
 
-class EditAdFragment : Fragment() {
+class EditAdFragment : BaseFragment(), AttributesAdapter.AttributesCallback {
 
+    private val viewModel by lazy { ViewModelProviders.of(this).get(EditAdViewModel::class.java) }
+    lateinit var imageAdapter: AdImagesAdapter
+    private val attributesAdapter by lazy { AttributesAdapter(viewModel.attributes, this) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +30,52 @@ class EditAdFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (arguments != null) {
+            val adUuid = EditAdFragmentArgs.fromBundle(arguments!!).adUuid
+            viewModel.setAdUuid(adUuid)
+            viewModel.uiState.observe(this, Observer { onUiStateChanged(it) })
+
+            attributesRv.adapter = attributesAdapter
+        } else {
+
+        }
+    }
+
+    override fun showLoading() {
+        containerNsv.visibility = View.GONE
+        loadingPb.visibility = View.VISIBLE
+    }
+
+    override fun showSuccess(dataMap: Map<String, Any>) {
+        containerNsv.visibility = View.VISIBLE
+        loadingPb.visibility = View.GONE
+        val ad = dataMap[DATA_AD_KEY] as Ad
+        loadAd(ad)
+    }
+
+    override fun showError(message: String) {
+    }
+
+    override fun showNoInternetConnection() {
+    }
+
+    private fun loadAd(ad: Ad) {
+        titleEt.setText(ad.title)
+        descriptionEt.setText(ad.description)
+        priceEt.setText(ad.price.toString())
+        priceWithDiscountEt.setText(ad.discountPrice.toString())
+        quantityEt.setText("No quantity")
+        categoriesTv.text = "category name"
+        subCategoriesTv.text = ad.subCategoryName
+        attributesAdapter.notifyDataSetChanged()
+    }
+
+    override fun onAttributePriceChanged(position: Int, price: String) {
+        viewModel.attributes[position].t = viewModel.attributes[position].t.copy(price = price.toInt())
+    }
+
+    override fun onAttributeChecked(position: Int, isChecked: Boolean) {
+        viewModel.attributes[position].t = viewModel.attributes[position].t.copy(isChecked = isChecked)
     }
 }
--- app/src/main/java/com/hmaserv/rz/ui/editAd/EditAdViewModel.kt
+++ app/src/main/java/com/hmaserv/rz/ui/editAd/EditAdViewModel.kt
@@ -1,7 +1,75 @@
 package com.hmaserv.rz.ui.editAd
 
-import com.hmaserv.rz.ui.BaseViewModel
+import com.hmaserv.rz.domain.*
+import com.hmaserv.rz.ui.NewBaseViewModel
+import com.hmaserv.rz.utils.Injector
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class EditAdViewModel : BaseViewModel() {
+const val DATA_AD_KEY = "ad"
 
+class EditAdViewModel : NewBaseViewModel() {
+
+    private val getAdUseCase = Injector.getAdUseCase()
+    private val getAttributesUseCase = Injector.getAttributesUseCase()
+
+    private var adUuid: String? = null
+    val attributes = ArrayList<AttributeSection>()
+
+    fun setAdUuid(adUuid: String) {
+        if (this.adUuid == null) {
+            this.adUuid = adUuid
+            getData()
+        }
+    }
+
+    override fun launchDataJob(): Job {
+        return scope.launch(dispatcherProvider.io) {
+            withContext(dispatcherProvider.main) { showDataLoading() }
+            if (adUuid != null) {
+                val adResult = getAdUseCase.getAd(adUuid!!)
+                val attributesResult = getAttributesUseCase.get("3f6a93ed-781b-459f-923e-9af386119690")
+                if (adResult is DataResource.Success && attributesResult is DataResource.Success) {
+                    val resultMap = mutableMapOf<String, ArrayList<Attribute.SubAttribute>>()
+                    attributesResult.data.forEach { main ->
+                        resultMap[main.name] = ArrayList(main.attributes)
+                    }
+                    adResult.data.mainAttributes.forEach { main ->
+                        val allSubAttributes = resultMap[main.name]
+                        main.attributes.forEach {sub ->
+                            for (i in 0 until (allSubAttributes?.size ?: 0)) {
+                                if (allSubAttributes?.get(i)?.name.equals(sub.name)) {
+                                    allSubAttributes?.removeAt(i)
+                                    allSubAttributes?.add(i, sub.copy(isChecked = true))
+                                }
+                            }
+                        }
+                    }
+
+                    for ((key, value) in resultMap) {
+                        attributes.add(AttributeSection(
+                            true,
+                            key
+                        ))
+                        value.forEach { sub ->
+                            attributes.add(AttributeSection(
+                                sub
+                            ))
+                        }
+                    }
+                    withContext(dispatcherProvider.main) { showSuccess(adResult.data) }
+                } else {
+                    withContext(dispatcherProvider.main) { showDataError() }
+                }
+            } else {
+                withContext(dispatcherProvider.main) { showDataError() }
+            }
+        }
+    }
+
+    private fun showSuccess(data: Ad) {
+        _uiState.value = UiState.Success(mapOf(Pair(DATA_AD_KEY, data)))
+    }
 }
