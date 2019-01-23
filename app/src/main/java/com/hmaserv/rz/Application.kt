ae2d57c0b0d0b012a8679e68ce7a0ec0d03b307b
elfiky <elfiky-work@hmaserv.com>
2019-01-23 13:47:22 +0200

    Update application class

--- app/src/main/java/com/hmaserv/rz/Application.kt
+++ app/src/main/java/com/hmaserv/rz/Application.kt
@@ -1,19 +1,13 @@
 package com.hmaserv.rz
 
 import android.app.Application
-import android.content.Context
-import android.util.Log
 import com.blankj.utilcode.util.Utils
 import com.facebook.spectrum.SpectrumSoLoader
-import com.facebook.spectrum.logging.SpectrumLogger
 import com.hmaserv.rz.domain.MyObjectBox
-import com.hmaserv.rz.utils.Constants
-import com.hmaserv.rz.utils.Injector
-import com.hmaserv.rz.utils.changeLanguage
 import io.objectbox.BoxStore
 import io.objectbox.android.AndroidObjectBrowser
 import timber.log.Timber
-import java.util.*
+import timber.log.Timber.DebugTree
 
 class RzApplication : Application() {
 
@@ -22,6 +16,9 @@ class RzApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
         boxStore = MyObjectBox.builder().androidContext(this).build()
         if (BuildConfig.DEBUG) {
             val started = AndroidObjectBrowser(boxStore).start(this)
