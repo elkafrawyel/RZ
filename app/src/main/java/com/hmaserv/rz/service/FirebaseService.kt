0f06c15f1bcc2e0ee672121133424a6622e771df
elfiky <elfiky-work@hmaserv.com>
2019-01-23 14:19:01 +0200

    Add firebase core, messaging and crashlytics

---
+++ app/src/main/java/com/hmaserv/rz/service/FirebaseService.kt
@@ -0,0 +1,12 @@
+package com.hmaserv.rz.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FirebaseService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+    }
+
+}
