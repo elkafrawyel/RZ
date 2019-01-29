a12e205e97f15897922cd2e5de5357572edb8604
elfiky <elfiky-work@hmaserv.com>
2019-01-29 13:54:59 +0200

    Add butterknife Update RzBaseFragment

--- app/src/main/java/com/hmaserv/rz/ui/RzBaseFragment.kt
+++ app/src/main/java/com/hmaserv/rz/ui/RzBaseFragment.kt
@@ -1,11 +1,14 @@
 package com.hmaserv.rz.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import butterknife.ButterKnife
 import com.hmaserv.rz.domain.Action
 import com.hmaserv.rz.domain.State
 import com.hmaserv.rz.domain.observeEvent
@@ -16,6 +19,7 @@ abstract class RzBaseFragment<T : State, M, VM : RzBaseViewModel<T, M>>(modelCla
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        ButterKnife.bind(this, view)
         viewModel.state.observe(this, Observer { renderState(it) })
         viewModel.messages.observeEvent(this, ::showMessage)
     }
--- app/src/main/java/com/hmaserv/rz/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/hmaserv/rz/ui/categories/CategoriesFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import butterknife.ButterKnife
+import butterknife.OnClick
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.hmaserv.rz.R
 import com.hmaserv.rz.domain.Action
@@ -16,9 +18,10 @@ import kotlinx.android.synthetic.main.categories_fragment.*
 
 class CategoriesFragment :
     RzBaseFragment<State.CategoriesState, String, CategoriesViewModel>(CategoriesViewModel::class.java),
-    SwipeRefreshLayout.OnRefreshListener {
+    SwipeRefreshLayout.OnRefreshListener,
+    BaseQuickAdapter.OnItemClickListener {
 
-    private val adapter = CategoriesAdapter()
+    private val categoriesAdapter = CategoriesAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,31 +32,9 @@ class CategoriesFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        backImgv.setOnClickListener { findNavController().navigateUp() }
-        searchMcv.setOnClickListener{openSearchFragment()}
-        categoriesRv.adapter = adapter
-        noConnectionCl.setOnClickListener { sendAction(Action.Started) }
-        errorCl.setOnClickListener { sendAction(Action.Started)  }
-        adapter.onItemClickListener =
-                BaseQuickAdapter.OnItemClickListener { _, _, position ->
-                    openSubCategories(adapter.data[position])
-                }
+        categoriesRv.adapter = categoriesAdapter
+        categoriesAdapter.onItemClickListener = this
         categoriesSwipe.setOnRefreshListener(this)
-    }
-
-    private fun openSearchFragment() {
-        findNavController().navigate(R.id.action_categoriesFragment_to_searchFragment)
-    }
-
-
-    private fun openSubCategories(category: Category) {
-        val action = CategoriesFragmentDirections.actionCategoriesFragmentToSubCategoriesFragment(
-            category.uuid,
-            category.title
-        )
-
-        findNavController().navigate(action)
     }
 
     override fun renderState(state: State.CategoriesState) {
@@ -63,11 +44,37 @@ class CategoriesFragment :
         emptyViewCl.visibility = state.emptyVisibility
         noConnectionCl.visibility = state.noConnectionVisibility
         errorCl.visibility = state.errorVisibility
-        adapter.replaceData(state.categories)
+        categoriesAdapter.replaceData(state.categories)
+    }
+
+    @OnClick(R.id.backImgv)
+    fun onBackClicked() {
+        findNavController().navigateUp()
+    }
+
+    @OnClick(R.id.searchMcv)
+    fun onSearchClicked() {
+        findNavController().navigate(R.id.action_categoriesFragment_to_searchFragment)
+    }
+
+    @OnClick(R.id.noConnectionCl, R.id.errorCl, R.id.emptyViewCl)
+    fun reload() {
+        sendAction(Action.Started)
     }
 
     override fun onRefresh() {
         sendAction(Action.Refresh)
+    }
+
+    override fun onItemClick(adapter: BaseQuickAdapter<*, *>?, view: View?, position: Int) {
+        val category = categoriesAdapter.data[position]
+        val action = CategoriesFragmentDirections
+            .actionCategoriesFragmentToSubCategoriesFragment(
+                category.uuid,
+                category.title
+            )
+
+        findNavController().navigate(action)
     }
 
 }
