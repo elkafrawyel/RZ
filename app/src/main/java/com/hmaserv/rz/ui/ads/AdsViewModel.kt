73b3a1882a7ebe540edc2ca362cca52d308d7d87
elfiky <elfiky-work@hmaserv.com>
2019-02-04 17:45:30 +0200

    Fix empty list in ads

--- app/src/main/java/com/hmaserv/rz/ui/ads/AdsViewModel.kt
+++ app/src/main/java/com/hmaserv/rz/ui/ads/AdsViewModel.kt
@@ -1,14 +1,12 @@
 package com.hmaserv.rz.ui.ads
 
 import android.view.View
-import com.blankj.utilcode.util.NetworkUtils
 import com.hmaserv.rz.R
 import com.hmaserv.rz.domain.*
 import com.hmaserv.rz.ui.RzBaseViewModel
 import com.hmaserv.rz.utils.Injector
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class AdsViewModel : RzBaseViewModel<State.AdsState, String>() {
 
@@ -21,7 +19,7 @@ class AdsViewModel : RzBaseViewModel<State.AdsState, String>() {
     var isList = true
 
     fun search(subUuid: String, searchText: String?) {
-        if (subCategoryUuid == null){
+        if (subCategoryUuid == null) {
             this.subCategoryUuid = subUuid
             this.searchText = searchText
             sendAction(Action.Started)
@@ -42,47 +40,55 @@ class AdsViewModel : RzBaseViewModel<State.AdsState, String>() {
     }
 
     private fun refreshData(isRefreshed: Boolean) {
-        if (NetworkUtils.isConnected()) {
-            if (adsJob?.isActive == true) {
-                return
-            }
+        checkNetwork(
+            job = adsJob,
+            success = { adsJob = launchAdsJob(isRefreshed) },
+            error = {
+                if (isRefreshed) {
+                    sendMessage(Injector.getApplicationContext().getString(R.string.label_no_internet_connection))
+                    sendState(
+                        State.AdsState(
+                            isRefreshing = false,
+                            dataVisibility = View.VISIBLE,
+                            ads = state.value?.ads ?: emptyList()
+                        )
+                    )
 
-            adsJob = launchAdsJob(isRefreshed)
-        } else {
-            if (isRefreshed) {
-                sendMessage(Injector.getApplicationContext().getString(R.string.label_no_internet_connection))
-                sendState(State.AdsState(
-                    isRefreshing = false,
-                    dataVisibility = View.VISIBLE,
-                    ads = state.value?.ads ?: emptyList()
-                ))
-
-            } else {
-                sendState(State.AdsState(noConnectionVisibility = View.VISIBLE))
+                } else {
+                    sendState(State.AdsState(noConnectionVisibility = View.VISIBLE))
+                }
             }
-        }
+        )
     }
 
     private fun launchAdsJob(isRefreshed: Boolean): Job {
         return launch(dispatcherProvider.io) {
-            withContext(dispatcherProvider.main) {
-                if (isRefreshed) {
-                    sendState(State.AdsState(isRefreshing = true))
-                } else {
-                    sendState(State.AdsState(loadingVisibility = View.VISIBLE))
+            if (isRefreshed) {
+                sendStateOnMain {
+                    State.AdsState(
+                        isRefreshing = true,
+                        dataVisibility = View.VISIBLE,
+                        ads = state.value?.ads ?: emptyList()
+                    )
                 }
+            } else {
+                sendStateOnMain { State.AdsState(loadingVisibility = View.VISIBLE) }
             }
             val result = getSearchUseCase.search(subCategoryUuid!!, searchText!!)
-            withContext(dispatcherProvider.main) {
-                when (result) {
-                    is DataResource.Success -> sendState(
-                        State.AdsState(
-                            dataVisibility = View.VISIBLE,
-                            ads = result.data
-                        )
-                    )
-                    is DataResource.Error -> sendState(State.AdsState(errorVisibility = View.VISIBLE))
+            when (result) {
+                is DataResource.Success -> {
+                    if (result.data.isEmpty()) {
+                        sendStateOnMain { State.AdsState(emptyVisibility = View.VISIBLE) }
+                    } else {
+                        sendStateOnMain {
+                            State.AdsState(
+                                dataVisibility = View.VISIBLE,
+                                ads = result.data
+                            )
+                        }
+                    }
                 }
+                is DataResource.Error -> sendStateOnMain { State.AdsState(errorVisibility = View.VISIBLE) }
             }
         }
     }
