aa7cd79e9412d8af1068bc6b0390bcff476aab7c
elfiky <elfiky-work@hmaserv.com>
2019-02-18 18:00:59 +0200

    Add contact us mock view

--- app/src/main/java/store/rz/app/ui/contactUs/ContactUsFragment.kt
+++ app/src/main/java/store/rz/app/ui/contactUs/ContactUsFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.contact_us_fragment.*
 
 import store.rz.app.R
 
@@ -16,6 +20,15 @@ class ContactUsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.contact_us_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sendMbtn.setOnClickListener {
+            Snackbar.make(view, "تم ارسال رسالتك بنجاح", Snackbar.LENGTH_SHORT).show()
+        }
+
+        backImgv.setOnClickListener { findNavController().navigateUp() }
     }
 
 }
